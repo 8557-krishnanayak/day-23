d5c3ba0f62e23cd5fa1ff800d973f818d9cb0cda
Krishna-Nayak603 <krishna.nayak@godigit.com>
2024-08-21 17:27:28 +0530

    Service

--- src/main/java/com/godigit/SpringEmployee/repository/EmployeeRepository.java
+++ src/main/java/com/godigit/SpringEmployee/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.godigit.SpringEmployee.repository;
 
 import com.godigit.SpringEmployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
---
+++ src/main/java/com/godigit/SpringEmployee/service/EmployeeService.java
@@ -0,0 +1,49 @@
+package com.godigit.SpringEmployee.service;
+
+import com.godigit.SpringEmployee.model.Employee;
+import com.godigit.SpringEmployee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    //    createEmployee,
+    public String createEmployee(Employee employee) {
+        employeeRepository.save(employee);
+        return "Insertion done";
+    }
+
+    // getEmployeeById
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No such user exist with this id"));
+    }
+
+    // getAllEmployees
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    // updateEmployee
+    public String updateEmployee(Long id, Employee e) {
+        Employee employee = getEmployeeById(id);
+        employee.setFirstName(e.getFirstName());
+        employee.setLastName(e.getLastName());
+        employeeRepository.save(employee);
+        return "changes done";
+    }
+
+    //deleteEmployee.
+    public String deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+        return "Employee id:\t" + id + " [DELETE]";
+    }
+}
